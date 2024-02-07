2e2cc59901680f7313cf05dbcbbd0d9742a00fcc
NhatLinh2003 <kingblacksss146@gmail.com>
2024-02-07 23:43:11 +0700

    intent login register

--- app/src/main/java/com/anpha/android_huflit/Login.java
+++ app/src/main/java/com/anpha/android_huflit/Login.java
@@ -2,13 +2,44 @@ package com.anpha.android_huflit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+     Button btnsignin,btnlogin;
+     EditText edtemaillogin,edtpasswordlogin;
+     TextView txtforgetpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addControlLogin();
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin = new Intent(Login.this, Register.class);
+                startActivity(signin);
+
+            }
+        });
+        txtforgetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forgotpassword = new Intent(Login.this,ForgotPassword.class);
+                startActivity(forgotpassword);
+            }
+        });
+    }
+    public void addControlLogin()
+    {
+        btnlogin =findViewById(R.id.btnlogin);
+        btnsignin=findViewById(R.id.btnsignup);
+        edtemaillogin=findViewById(R.id.edtemaillogin);
+        edtpasswordlogin=findViewById(R.id.edtpasswordlogin);
+        txtforgetpassword = findViewById(R.id.txtforgetpassword);
     }
 }
