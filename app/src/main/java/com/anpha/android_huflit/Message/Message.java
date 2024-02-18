5c4b92b43c2b8bc2af7ad52dbc3b4e0c4c155cf2
Tuna004 <hoangtuanle2004@gmail.com>
2024-02-18 17:46:07 +0700

    chat view completed

---
+++ app/src/main/java/com/anpha/android_huflit/Message/Message.java
@@ -0,0 +1,27 @@
+package com.anpha.android_huflit.Message;
+
+    public class Message {
+        private String text;
+        private boolean sentByUser;
+    
+        public Message(String text, boolean sentByUser) {
+            this.text = text;
+            this.sentByUser = sentByUser;
+        }
+    
+        public String getText() {
+            return text;
+        }
+    
+        public void setText(String text) {
+            this.text = text;
+        }
+    
+        public boolean isSentByUser() {
+            return sentByUser;
+        }
+    
+        public void setSentByUser(boolean sentByUser) {
+            this.sentByUser = sentByUser;
+        }
+    }
