d9936e4222cd9c017f188682f74b40dba07206bb
Hulenn <luuhonghuu530@gmail.com>
2024-02-28 21:36:04 +0700

    thêm themeview + profileview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //thư viện chỉnh độ tròn của ảnh
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/anpha/android_huflit/PopupOption.java
+++ app/src/main/java/com/anpha/android_huflit/PopupOption.java
@@ -3,6 +3,10 @@ package com.anpha.android_huflit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.nio.BufferUnderflowException;
 
 public class PopupOption extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class PopupOption extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup_option);
+        Bundle bundle=getIntent().getExtras();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mnu=getMenuInflater();
+        mnu.inflate(R.menu.profile_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
