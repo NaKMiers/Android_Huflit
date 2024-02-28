1bb6230510a4cee152f145fd30d8d2667d0598e1
Tuna004 <{hoangtuanle2004@gmail.com}>
2024-02-28 17:13:03 +0700

    okk

--- app/src/main/java/com/anpha/android_huflit/ImageChat.java
+++ app/src/main/java/com/anpha/android_huflit/ImageChat.java
@@ -1,5 +1,6 @@
 package com.anpha.android_huflit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
@@ -378,5 +379,8 @@ public class ImageChat extends AppCompatActivity {
     }
 
 
-
+    public void handleChangeChatMode(View view) {
+        Intent intent = new Intent(ImageChat.this,TextChat.class);
+        startActivity(intent);
+    }
 }
